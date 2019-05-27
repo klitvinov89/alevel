16a52f40452657064af4aa24c620595d40f5038d
Koss <k.litvinov89@gmail.com>
2019-05-27 21:00:08 +0300

    hw4(1)

--- Litvinov_HW/Auto/HW3/HomeWork3_4.java
+++ Litvinov_HW/Auto/HW3/HomeWork3_4.java
@@ -1,32 +1,85 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeWork3_4 {
-    public static void main(String[] arg) {
-        String[] cities = new String []{"Москва", "Амстердам", "Минск", "Курск", "Киев"};
+    public static void main(String[] arg) throws IOException {
+        Scanner scannerObj;
+        String userCity;
+        String empty = "null";
+        boolean flagForWhile = true;
+        boolean firstCircle= true;
+        char firstChar = 0;
+        char lastChar = 0;
+        char a ;
+        String[] cities = {"Москва", "Амстердам", "Минск", "Курск", "Киев"};
         System.out.println(Arrays.toString(cities));
         System.out.println("Давай поиграем в ‘Города’. Ты первый называй!");
-        Scanner scannerObj = new Scanner(System.in);
-        String usercity = scannerObj.nextLine();
-        System.out.println("Вы назвали город "+usercity);
-//        String buf;
-        for (int i=0; i<cities.length-1;i++){
-            if (cities[i]==usercity){
-                cities[i]="null";break;}
-                System.out.println(Arrays.toString(cities));
-//                    String[] newcities = new String[cities.length-1];
-//                        for (int j=0; j<newcities.length-1;j++){
-//                            if (newcities[j]!=usercity){
-//                                newcities[j]=cities[i];
-//                                System.out.println(newcities);
-                            }
+
+        while(flagForWhile==true) {
+            if(firstCircle==false){
+                scannerObj = new Scanner(System.in);
+                userCity = scannerObj.nextLine();
+                firstChar = userCity.toLowerCase().charAt(0);
+                if(firstChar==lastChar){
+                    a = userCity.toLowerCase().charAt(userCity.length() - 1);
+                    System.out.println("Вы назвали город " + userCity);
+                    for (int i = 0; i < cities.length; i++) {
+                        if (cities[i].toLowerCase().equals(userCity.toLowerCase())) {
+                            cities[i]=empty;
+                        }
+                    }
+                    System.out.println("Значит мне на " + a);
+                    for (int k = 0; k < cities.length; k++) {
+                        if (cities[k].toLowerCase().charAt(0) == a) {
+                            System.out.println("Тогда я называю " + cities[k]);
+                            lastChar = cities[k].toLowerCase().charAt(cities[k].length()-1);
+                            System.out.println("Значит тебе на " + lastChar);
+                            cities[k]=empty;
+                            flagForWhile=true;
+                            break;
+                        } else {
+                            flagForWhile=false;
+                        }
+                    }
+                    firstCircle = false;
+                }
+                else{
+                    System.out.println("Вы назвали город не на ту букву!");
+                    flagForWhile=true;
+                }
             }
-//        }
-//        char a = usercity.toLowerCase().charAt(usercity.length()-1);
-//        System.out.println("Значит мне на "+a);
-//            for (int k=0; k<cities.length-1;k++){
-//                if (cities[k].toLowerCase().charAt(0)==a){
-//                    System.out.println("Тогда я называю " +cities[k]);
-//                    break;
-//                }
+            else {
+                scannerObj = new Scanner(System.in);
+                userCity = scannerObj.nextLine();
+                a = userCity.toLowerCase().charAt(userCity.length() - 1);
+                System.out.println("Вы назвали город " + userCity);
+
+                for (int i = 0; i < cities.length; i++) {
+                    if (cities[i].toLowerCase().equals(userCity.toLowerCase())) {
+                        cities[i]=empty;
+                    }
+                }
+
+                System.out.println("Значит мне на " + a);
+                for (int k = 0; k < cities.length; k++) {
+                    if (cities[k].toLowerCase().charAt(0) == a) {
+                        System.out.println("Тогда я называю " + cities[k]);
+                        lastChar = cities[k].toLowerCase().charAt(cities[k].length()-1);
+                        System.out.println("Значит тебе на " + lastChar);
+                        cities[k]=empty;
+                        flagForWhile=true;
+                        break;
+                    }
+                    else {
+                        flagForWhile=false;
+                    }
+                }
+                firstCircle = false;
             }
+        }
+
+        System.out.println("Я проиграл, вы знаете больше городов!");
+    }
+}
